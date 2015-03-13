fb68e6759cb3e70c5c64c37fb81704b853498492
840619 <sravani6793@gmail.com>
2015-03-13 11:30:57 +0530

    ddkd

--- Gemfile
+++ Gemfile
@@ -7,9 +7,12 @@ gem 'rails', '4.2.0'
 gem 'bootstrap-sass', '3.2.0.0'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
+gem 'bcrypt', '~> 3.1.7'
+gem 'faker'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
-
+gem 'will_paginate',           '3.0.7'
+gem 'bootstrap-will_paginate', '0.0.10'
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
 # Use CoffeeScript for .coffee assets and views
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,23 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
+  get 'sessions/new'
+
   get 'users/new'
 
  root 'static_pages#home'
  get  'static_pages/home'
-
+  get  'static_pages/help'
   get  'static_pages/about'
   get  'static_pages/contact'
   get 'signup'  => 'users#new'
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
   resources:users
+  resources :account_activations, only: [:edit]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
